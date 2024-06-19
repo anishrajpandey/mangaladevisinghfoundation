64bcceb53d8be3c49929f628df948aaa01d19800
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-06-19 13:59:12 +0545

    added emailjs

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,30 @@
-import React from "react";
+"use client";
 
+import React, { useRef, useState } from "react";
+import emailjs, { send } from "@emailjs/browser";
 const Contact = () => {
+  const [loading, setLoading] = useState(false);
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    const publicKey = "f1KfnE6PoOqPR0Vx-";
+    const templateID = "template_6kfldkm";
+    const serviceID = "service_89lxfpi";
+    console.log(formRef.current);
+    emailjs
+      .sendForm(serviceID, templateID, formRef?.current || "", {
+        publicKey,
+      })
+      .then(() => {
+        // window.location.href = "/contact";
+        alert("Successfully sent message");
+        setLoading(false);
+      })
+      .catch((err) => {
+        alert(err.text);
+      });
+  };
+  const formRef = useRef(null);
   return (
     <>
       <section className="relative z-10 overflow-hidden bg-white py-24 lg:py-[120px]">
@@ -116,34 +140,35 @@ const Contact = () => {
             </div>
             <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
               <div className="relative rounded-lg bg-white p-8 shadow-lg 2 sm:p-12">
-                <form>
+                <form ref={formRef} onSubmit={sendEmail}>
                   <ContactInputBox
                     type="text"
-                    name="name"
+                    name="from_name"
                     placeholder="Your Name"
                   />
                   <ContactInputBox
                     type="text"
-                    name="email"
+                    name="from_email"
                     placeholder="Your Email"
                   />
                   <ContactInputBox
                     type="text"
-                    name="phone"
+                    name="from_number"
                     placeholder="Your Phone"
                   />
                   <ContactTextArea
                     row="6"
                     placeholder="Your Message"
-                    name="details"
+                    name="message"
                     defaultValue=""
                   />
                   <div>
                     <button
                       type="submit"
+                      disabled={loading}
                       className="w-full rounded border border-primary bg-primary p-3 text-white bg-amber-500 transition hover:bg-opacity-90"
                     >
-                      Send Message
+                      {loading ? "Loading... " : "Send Message"}
                     </button>
                   </div>
                 </form>
@@ -967,6 +992,30 @@ const Contact = () => {
             loading="lazy"
           ></iframe>
         </div>
+        {loading && (
+          <div
+            // role="status"
+            className="w-screen h-screen flex justify-center items-center fixed top-0 left-0 z-30 bg-red-50 bg-opacity-35"
+          >
+            <svg
+              aria-hidden="true"
+              className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        )}
       </section>
     </>
   );
